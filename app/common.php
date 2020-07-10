2b285895ed378f5c9df6fe8c6c26d68c606c5c73
bxd <1021714291@qq.com>
2020-07-10 14:19:14 +0800

    添加常用的common里面的函数

--- app/common.php
+++ app/common.php
@@ -238,3 +238,99 @@ function http_get($url, $timeout = 10, $header = false)
         return $resposne;
     }
 }
+
+/**
+ * 数据脱敏
+ * @param  string  $string  需要脱敏值
+ * @param  int  $start  开始
+ * @param  int  $length  结束
+ * @param  string  $re  脱敏替代符号
+ * @return bool|string
+ * 例子:
+ * dataDesensitization('18811113683', 3, 4); //188****3683
+ * dataDesensitization('乐杨俊', 0, -1); //**俊
+ */
+function dataDesensitization($string, $start = 0, $length = 0, $re = '*')
+{
+    if (empty($string)) {
+        return false;
+    }
+    $strarr = array();
+    $mb_strlen = mb_strlen($string);
+    while ($mb_strlen) {//循环把字符串变为数组
+        $strarr[] = mb_substr($string, 0, 1, 'utf8');
+        $string = mb_substr($string, 1, $mb_strlen, 'utf8');
+        $mb_strlen = mb_strlen($string);
+    }
+    $strlen = count($strarr);
+    $begin = $start >= 0 ? $start : ($strlen - abs($start));
+    $end = $last = $strlen - 1;
+    if ($length > 0) {
+        $end = $begin + $length - 1;
+    } elseif ($length < 0) {
+        $end -= abs($length);
+    }
+    for ($i = $begin; $i <= $end; $i++) {
+        $strarr[$i] = $re;
+    }
+    if ($begin >= $end || $begin >= $last || $end > $last) {
+        return false;
+    }
+    return implode('', $strarr);
+}
+
+/**
+ *  用户密码加密
+ * @param $str
+ * @return  string
+ */
+function generateUserPassword($str)
+{
+    return md5(md5($str));
+}
+
+/**
+ *  哈希密码加密
+ * @param $str
+ * @return bool|string
+ */
+function getHashPassword($str)
+{
+    return password_hash($str, PASSWORD_DEFAULT);
+}
+
+/**
+ *  判断是否来自微信
+ * @return string
+ */
+function isFromWeixin()
+{
+    return isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'],
+        'MicroMessenger') !== false ? true : false;
+}
+
+/**
+ *  判断是否来自Iphone
+ * @return string
+ */
+function isIphone()
+{
+    if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+        return false;
+    }
+    $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+    if (strpos($agent, "iphone") || strpos($agent, "ipad")) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * 判断是否来自支付宝
+ * @return string
+ */
+function isAlipay()
+{
+    return isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'],
+        'AlipayClient') !== false ? true : false;
+}
